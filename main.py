c022f7582ccfca113d29f65eb6f203d8d9eb7bca
adambanner <adamajbbanner@hotmail.co.uk>
2023-04-21 09:25:47 +0100

    Currently adding comments and making it more into a function to make it resuaseable

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ row_string = row_start + ':' + row_end
 column_names = ['Comments', 'Service', 'Able', 'Solution',
                 'Cat', 'Notes']
 
-def find_cat_from
-    """Reads a file using custom columns and names them with a 
-    given set of names in the form of a list"""
-    data = pd.read_excel(file, usecols=row_string,
-                         names=column_names)
 
-    """I have to select a specific range of these 
+"""I feel like this is going to come in more useful down the line. 
+This could split them up by category based upon the current naming
+scheme and we can send the relevant people the comments related to their departments. 
+Developers get techincal problems. Designers get journey problems. HMRC get amount issues"""
+def label_data(data, letter):
+    """I have to select a specific range of these
     ones because I havent done all of the labelling 
     and I switched conventions at index 125. This is 
     specific to this file and to make
     it reproducible the format must be standardised.
-    
+
     It then resets the index to help the iteration"""
     data = data.iloc[125:433].dropna(subset='Cat').reset_index()
     """Creates a series out of the label columns"""
@@ -34,28 +34,34 @@ def find_cat_from
 
     """For each row in the series"""
     for i in series:
-
         """Append the value of whether letter is in that row
         (string etc 'JTCIA')"""
-        labels.append(('J' in i))
+        labels.append((letter in i))
 
     """The new data frame with only those where the """
-    reversed_labels = [not label for label in labels]
+    data['labels'] = labels
 
-    false_data = data[reversed_labels]
-    truth_data = data[labels]
-
-
-
+    """Return the data with an added column that is true if that row is in the category
+    and 0 if it is not. """
+    return data
 
 
+"""Reads a file using custom columns and names them with a 
+given set of names in the form of a list"""
+data = pd.read_excel(file, usecols=row_string,
+                     names=column_names)
 
+"""The function now returns data associated with a particular category
+T = technology
+J = Journey
+I = Information
+A = Amount
+C = Communication
 
-
-
-
-
-
+Returns a data frame that has a list of boolean values indicating if it belongs to the category."""
+technical_data = label_data(data, 'T')
+technical_data.drop(columns=['index', 'Notes', 'Solution', 'Able', 'Cat'], inplace=True)
+print(technical_data.columns, technical_data.shape)
 
 
 # with pd.ExcelWriter('journey_comments.xlsx', engine='openpyxl') as writer:
